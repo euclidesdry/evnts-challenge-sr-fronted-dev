1ee08aa75c2b8a0f53dbb61396e6dfcccbd0a5b7
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-04 23:06:12 +0100

    refactor(app): ♻️ cleaning the project

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: Number(process.env.VITE_PORT) || 3000,
-  },
+	plugins: [react()],
+	server: {
+		host: true,
+		port: Number(process.env.VITE_PORT) || 3000,
+		watch: {
+			usePolling: true,
+		},
+	},
 });
