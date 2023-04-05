33f34b5412f672b2e4f5556ffa449b622de4d609
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-05 20:45:01 +0100

    feat(app): ✨ adding Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export default function Footer() {
+	return <Container className="App-footer">©2023 All Rights Reserved to @euclidesdry</Container>;
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+	width: 100%;
+	padding: 16px;
+	text-align: center;
+	padding-top: 32px;
+	padding-bottom: 64px;
+`;
