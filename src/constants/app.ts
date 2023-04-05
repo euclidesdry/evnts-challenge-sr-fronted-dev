159cfb74cf77d0224508518d346757581e22bb3b
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-05 09:28:34 +0100

    feat(constants): ✨ adding app constrants

---
+++ src/constants/app.ts
@@ -0,0 +1,4 @@
+export const FILTER_HEIGHT = "48px";
+export const BASE_DROP_SHADOW = "0px 4px 8px rgba(0, 0, 0, 0.18)";
+export const SMALL_ROUNDED_BORDER = "16px";
+export const BASE_BORDER_RADIUS = "32px";
