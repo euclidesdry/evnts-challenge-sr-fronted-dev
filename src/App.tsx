9a7db38ab810f18e035b6851ff5a3433ab33a06e
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-08 03:30:15 +0100

    refactor(app): ♻️ disabling refletch on Window Focus query client option

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,27 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import { AppContextProvider } from "./contexts/App";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false, // default: true
+		},
+	},
+});
 
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<div className="App">
-				<Layout>
-					<Home />
-				</Layout>
-			</div>
-			<ReactQueryDevtools initialIsOpen={false} />
+			<AppContextProvider>
+				<div className="App">
+					<Layout>
+						<Home />
+					</Layout>
+				</div>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</AppContextProvider>
 		</QueryClientProvider>
 	);
 }
