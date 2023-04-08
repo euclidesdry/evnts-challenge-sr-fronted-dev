3fd6072c8d5c50bc27c06852e2a14132cd7299b8
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-08 04:05:32 +0100

    feat(component): ✨ refactoring pokemon list logic

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,17 +1,35 @@
 import { PokemonCard } from "./components";
 import { ListPokemonsType } from "../../types/services/pokemon";
 
-import { Container } from "./styles";
+import { Container, Loader, NoCards } from "./styles";
+import { SpinnerCircular } from "spinners-react";
 
 type PokemonListProps = {
 	data?: ListPokemonsType["results"];
+	loading?: boolean;
+	searchTerm: string;
+	isSearchError: boolean;
 };
 
-export default function PokemonList({ data }: PokemonListProps) {
+export default function PokemonList({ data, searchTerm, loading, isSearchError }: PokemonListProps) {
+	const hasNoCards = isSearchError || Boolean(loading) || Boolean(data && data?.length <= 0);
+
 	return (
-		<Container noCards={!data}>
-			{data && data?.map(({ name, url }) => <PokemonCard key={name} url={url} name={name} />)}
-			{!data && <p style={{ textAlign: "center", width: "100%" }}>Loading...</p>}
+		<Container noCards={hasNoCards}>
+			{!loading && !isSearchError
+				? data?.map(({ name, url }) => <PokemonCard key={name} url={url} name={name} />)
+				: null}
+			{loading && (
+				<Loader>
+					<SpinnerCircular enabled={true} color="#222222" size="38" />
+					<p>Loading...</p>
+				</Loader>
+			)}
+			{hasNoCards && !loading && (
+				<NoCards>
+					<p>No cards to show</p>
+				</NoCards>
+			)}
 		</Container>
 	);
 }
