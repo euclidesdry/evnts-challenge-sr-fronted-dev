33e26ca7da445017db212d84c68f1f70e90fa77d
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-08 04:03:11 +0100

    feat(app): ✨ adding filter

--- src/components/Header/components/PokemonFilter/PokemonFilter.tsx
+++ src/components/Header/components/PokemonFilter/PokemonFilter.tsx
@@ -1,36 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
 import { Container, List } from "./styles";
-
-const Pokemons = [
-	"all",
-	"normal",
-	"fire",
-	"water",
-	"electric",
-	"grass",
-	"ice",
-	"fighting",
-	"poison",
-	"ground",
-	"flying",
-	"psychic",
-	"bug",
-	"rock",
-	"ghost",
-	"dragon",
-	"dark",
-	"steel",
-	"fairy",
-];
+import { listPokemonsType } from "~/services/pokemon";
+import { useAppContext } from "~/contexts/App";
+import { convertToPokemonURL, getIdFromURL } from "~/helpers/url";
 
 export default function PokemonFilter() {
+	const { selectedType, setSearchTerm, setOffset, setSelectedType } = useAppContext();
+
+	const { data: pokemonTypeList } = useQuery(["pokemon-types"], () => listPokemonsType(), {
+		staleTime: 1000 * 60 * 30, // 1000ms * 60s * 30min = 30min
+	});
+
+	function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+		setSelectedType(e.target.value === "all" ? null : e.target.value);
+		setOffset(0);
+		setSearchTerm("");
+	}
+
 	return (
 		<Container>
-			<List name="pokemon-type">
-				{Array.from(Pokemons).map((item) => (
-					<option key={item} value="item" style={{ textTransform: "capitalize" }}>
-						{item}
-					</option>
-				))}
+			<List name="pokemon-type" defaultValue="all" value={selectedType || ""} onChange={handleChange}>
+				<option value="all">All Types</option>
+				{pokemonTypeList &&
+					pokemonTypeList.map(({ name, url }) => (
+						<option key={name} value={getIdFromURL(convertToPokemonURL(url))} style={{ textTransform: "capitalize" }}>
+							{name}
+						</option>
+					))}
 			</List>
 		</Container>
 	);
