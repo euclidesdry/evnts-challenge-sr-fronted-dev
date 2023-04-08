5a0ff5bb9876a02bfb7e48d7cfc3edc6a1977e43
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-08 03:31:23 +0100

    test(mocks): ✨ adding list pokemon types endpoint handler

--- jest/mocks/server/handlers.ts
+++ jest/mocks/server/handlers.ts
@@ -1,8 +1,11 @@
 import { rest } from "msw";
 import { BASE_URL } from "../../../src/services/config";
-import { pokemonDetailsBuilder } from "../../../src/__mocks__/pokemon";
+import { pokemonDetailsBuilder, pokemonTypesBuilder } from "../../../src/__mocks__/pokemon";
 
 export const handlers = [
+	rest.get(`${BASE_URL}/type`, async (_, res, ctx) => {
+		return res(ctx.status(200), ctx.json(pokemonTypesBuilder()));
+	}),
 	rest.get<{ id: string }>(`${BASE_URL}/pokemon/:id`, async (req, res, ctx) => {
 		const { id: selectedId } = req.params;
 		const id = Number(selectedId);
