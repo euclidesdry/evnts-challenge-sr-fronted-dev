793361dca553b4d84e97e097a07718d7b7dd1f07
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-08 04:03:36 +0100

    feat(component): ✨ adding search bar

--- src/components/Header/components/SearchBar/SearchBar.tsx
+++ src/components/Header/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,27 @@
+import { useForm } from "react-hook-form";
 import PokeLogo from "../../../../assets/icons/pokemon-logo.svg";
-import { Container, SearchButton, SearchInput } from "./styles";
+import { ClearSearchBarButton, Container, SearchButton, SearchInput } from "./styles";
+import { useAppContext } from "~/contexts/App";
 
 export default function SearchBar() {
+	const { searchTerm, setSearchTerm } = useAppContext();
+
+	const { register, handleSubmit, setValue, getValues } = useForm();
+
+	function handleSearch(values: any) {
+		setSearchTerm(values.searchBar as string);
+	}
+
+	function handleClearSearch() {
+		setValue("searchBar", "");
+		setSearchTerm("");
+	}
+
 	return (
 		<Container>
-			<SearchInput type="text" name="search-pokemon" placeholder="Search your Pokemon!" />
-			<SearchButton>
+			<SearchInput {...register("searchBar")} type="search" placeholder="Search your Pokemon!" />
+			{searchTerm?.length > 0 && <ClearSearchBarButton onClick={handleClearSearch}>X</ClearSearchBarButton>}
+			<SearchButton onClick={handleSubmit(handleSearch)}>
 				<img src={PokeLogo} alt="Search Pokemon Logo" />
 			</SearchButton>
 		</Container>
