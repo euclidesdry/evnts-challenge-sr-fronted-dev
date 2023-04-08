b987c8d6926cb3b3ce65a46d29ef13a0b5eb9228
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-08 04:06:06 +0100

    feat(constants): ✨ adding pokemon types constant

--- src/constants/mocks/server/pokemon.ts
+++ src/constants/mocks/server/pokemon.ts
@@ -1,6 +1,17 @@
 import { BASE_URL } from "~/services";
-import { PokemonDetailsType } from "../../../../src/types/services/pokemon";
+import { ListPokemonsType, PokemonDetailsType } from "../../../../src/types/services/pokemon";
 import { RecursivePartial } from "../../../../src/utils/types";
+
+export const pokemonTypes: ListPokemonsType["results"] = [
+	{
+		name: "normal",
+		url: "https://pokeapi.co/api/v2/type/1/",
+	},
+	{
+		name: "fighting",
+		url: "https://pokeapi.co/api/v2/type/1/",
+	},
+];
 
 export const pokemonDetails: RecursivePartial<PokemonDetailsType> = {
 	id: 395,
