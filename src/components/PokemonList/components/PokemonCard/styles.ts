c53fcca912b1ea4f42ad5d2fee8e62158b3c540e
Euclides do Rosário <euclidesdry@gmail.com>
2023-04-08 04:06:50 +0100

    feat(styles): 💄 adding cards animation

--- src/components/PokemonList/components/PokemonCard/styles.ts
+++ src/components/PokemonList/components/PokemonCard/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { BASE_BORDER_RADIUS, BASE_DROP_SHADOW } from "../../../../constants";
 
 export const Container = styled.div`
+	max-height: 140px;
 	position: relative;
 	background-color: #fff;
 	padding: 16px;
@@ -9,9 +10,23 @@ export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	transition: all 0.3s ease;
 
 	border-radius: ${BASE_BORDER_RADIUS};
 	box-shadow: ${BASE_DROP_SHADOW};
+
+	&:hover {
+		box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
+		transform: scale(1.1);
+		cursor: pointer;
+		user-select: none;
+		z-index: 99;
+	}
+
+	&:active {
+		transition: all 0.2s ease;
+		transform: scale(0.95);
+	}
 `;
 
 export const Sprite = styled.img`
